32fc2a6ac283abcb56d019ed722f1db0804c86be
jagjitkaur0000 <jagjitkaur000000@gmail.com>
2026-03-27 01:03:03 +0530

    Fix CORS for Vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,15 +6,15 @@ from app.api.routes.company import router as company_router
 from app.api.routes.job import router as job_router
 from app.api.routes.application import router as application_router
 
-
 app = FastAPI(title="Job Board API")
-
 
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://job-board-z8bn.onrender.com",
+    "https://job-board-brown-theta.vercel.app"
 ]
 
 app.add_middleware(
@@ -25,13 +25,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get("/")
 def root():
     return {"message": "Job Board API running"}
 
-
-app.include_router(auth_router)          
-app.include_router(company_router)       
-app.include_router(job_router)           
-app.include_router(application_router)   
+app.include_router(auth_router)
+app.include_router(company_router)
+app.include_router(job_router)
+app.include_router(application_router)
